c5f91d86fe0b05721025743debbc9412281f6d1a
sostdk <morten@sost.dk>
2018-04-09 09:08:18 +0200

    dtdt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-$(document).ready(function(){
-  var elem = document.querySelector('.sidenav');
-  var instance = M.Sidenav.init(elem, options);
-
-});
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
